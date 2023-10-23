418c70812769cdac862be256b5c645f4b9fd4a7f
Yana Shurinova <yana.shurinova009@gmail.com>
2023-10-23 18:11:39 +0000

    Shift1061. Обеспечить простейшую работу эндпоинта сокращения ссылок

--- src/app/crud.py
+++ src/app/crud.py
@@ -1,4 +1,8 @@
 """Модуль для обработки запросов."""
+import random
+from string import ascii_lowercase, ascii_uppercase, digits
+
+from src.app.database import db_short_url, db_url
 from src.app.schemas import URL
 
 
@@ -11,4 +15,10 @@ async def shorter_url(inp_url: URL) -> str:
     Returns:
         str: сокращенная ссылка
     """
-    return ' '
+    address = '/'.join(inp_url.url.split('/')[:3])
+    long_id = '/'.join(inp_url.url.split('/')[3:])
+    db_url.update({long_id: address})
+    new_address = 'http://localhost:24023'
+    new_id = ''.join(random.choices(ascii_uppercase + digits + ascii_lowercase, k=10))  # noqa: S311, WPS221, E501
+    db_short_url.update({long_id: [new_id, new_address]})
+    return new_address + '/' + new_id  # noqa: WPS336
